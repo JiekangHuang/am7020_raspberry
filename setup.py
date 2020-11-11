dc7955ebdad52224ef16a8a91fa361ce929b4260
Zack Huang <zack@atticedu.com>
2020-11-11 15:12:20 +0800

    add file

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# setup.py
+# @Author : Zack Huang ()
+# @Link   : zack@atticedu.com
+# @Date   : 2020/11/11 下午2:03:50
+
+import setuptools
+
+# with open("README.md", "r") as fh:
+    # long_description = fh.read()
+
+setuptools.setup(
+    name="am7020",
+    version="0.0.1",
+    author="Zack Huang",
+    author_email="zack@atticedu.com",
+    description="AT Command library dedicated to am7020 http mqtt",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/JiekangHuang/am7020_raspberry",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
